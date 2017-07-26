5726012b81d6f77b25cad959251337f619a6170a
Felipe Monteiro <felipe.monteiro@att.com>
2017-07-26 14:24:46 +0100

    Add base deckhand exception.

--- deckhand/errors.py
+++ deckhand/errors.py
@@ -12,6 +12,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+class DeckhandException(Exception):
+    """Base Deckhand Exception
+    To correctly use this class, inherit from it and define
+    a 'msg_fmt' property. That msg_fmt will get printf'd
+    with the keyword arguments provided to the constructor.
+    """
+    msg_fmt = "An unknown exception occurred."
+    code = 500
+
+    def __init__(self, message=None, **kwargs):
+        self.kwargs = kwargs
+
+        if 'code' not in self.kwargs:
+            try:
+                self.kwargs['code'] = self.code
+            except AttributeError:
+                pass
+
+        if not message:
+            try:
+                message = self.msg_fmt % kwargs
+
+            except Exception:
+                message = self.msg_fmt
+
+        self.message = message
+        super(DeckhandException, self).__init__(message)
+
+    def format_message(self):
+        return self.args[0]
+
 
 class DeckhandException(Exception):
     """Base Nova Exception
