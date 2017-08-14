52c01ea712fd7b6a2508d5a257f57bd90f81d8c4
Felipe Monteiro <felipe.monteiro@att.com>
2017-08-14 15:15:15 +0100

    Improve error handling and content type enforcement
    This commit improves error handling and content type enforcement
    in Deckhand by doing the following:
    
      - Removes ``return self.return_error(...)`` pattern from the
        code because falcon already supports built-in exception types
        that are more descriptive and standardized. Further, raising
        an exception is a better pattern than having to return
        prematurely using ``return`` (as raising an exception can
        bubble up and escape the current code block).
      - Adds a new decorator called ``enforce_content_types`` which
        raises a 415 if the Content-Type isn't application/x-yaml.
    
    Included helpers added:
    
      - to_python_object: Convert Falcon request YAML into Python object.
    
    Change-Id: I6a54b76f850ba920734c0433623bda5016eb4bf0

---
+++ deckhand/tests/functional/test_revisions.py
@@ -0,0 +1,46 @@
+# Copyright 2017 AT&T Intellectual Property.  All other rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import yaml
+
+import falcon
+
+from deckhand.tests.functional import base as test_base
+
+
+class TestRevisionDocumentsApi(test_base.TestFunctionalBase):
+
+    def test_show_revision(self):
+        created_document = self.create_document('sample_document')[0]
+        revision_id = created_document['revision_id']
+
+        resp = self.app.simulate_get(
+            '/api/v1.0/revisions/%s' % revision_id)
+        self.assertEqual(falcon.HTTP_200, resp.status)
+
+    def test_list_revisions(self):
+        created_document = self.create_document('sample_document')[0]
+        revision_id = created_document['revision_id']
+
+        resp = self.app.simulate_get('/api/v1.0/revisions')
+        retrieved_revisions = yaml.safe_load(resp.text)
+        self.assertIn('results', retrieved_revisions)
+        self.assertEqual(1, len(retrieved_revisions['results']))
+        self.assertEqual(1, retrieved_revisions['count'])
+
+        retrieved_revision = retrieved_revisions['results'][0]
+        for attr in ('count', 'createdAt', 'id'):
+            self.assertIn(attr, retrieved_revision)
+        self.assertEqual(revision_id, retrieved_revision['id'])
+        self.assertEqual(1, retrieved_revision['count'])
