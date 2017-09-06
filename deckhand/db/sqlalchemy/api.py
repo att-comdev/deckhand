038fee68808a7db4cc2740101b5764e066dfd36a
Felipe Monteiro <felipe.monteiro@att.com>
2017-09-06 21:27:44 +0100

    Revision diffing API
    This commit implements revision diffing API and the
    required back-end logic needed to realize it.
    
    This commit includes unit tests and unskips as many
    of the functional tests related to revision diffing as
    possible. This includes all functional tests except for
    those that have to do with document deletion, which
    will be implemented in a separate commit.
    
    Change-Id: I0419ee9b8cf3fca6fe75818615d2338dc00b1003

--- deckhand/db/sqlalchemy/api.py
+++ deckhand/db/sqlalchemy/api.py
@@ -172,14 +172,16 @@ def _documents_create(values_list, session=None):
 
 def document_get(session=None, raw_dict=False, **filters):
     session = session or get_session()
-    if 'document_id' in filters:
-        filters['id'] = filters.pop('document_id')
 
-    try:
-        document = session.query(models.Document)\
-            .filter_by(**filters)\
-            .one()
-    except sa_orm.exc.NoResultFound:
+    # Retrieve the most recently created version of a document. Documents with
+    # the same metadata.name and schema can exist across different revisions,
+    # so it is necessary to use `first` instead of `one` to avoid errors.
+    document = session.query(models.Document)\
+        .filter_by(**filters)\
+        .order_by(models.Document.created_at.desc())\
+        .first()
+
+    if not document:
         raise errors.DocumentNotFound(document=filters)
 
     return document.to_dict(raw_dict=raw_dict)
@@ -260,28 +262,41 @@ def revision_delete_all(session=None):
         .delete(synchronize_session=False)
 
 
-def revision_get_documents(revision_id, session=None, **filters):
+def revision_get_documents(revision_id, session=None, include_history=False,
+                           **filters):
     """Return the documents that match filters for the specified `revision_id`.
 
     Deleted documents are not included unless deleted=True is provided in
     ``filters``.
 
+    :param revision_id: The ID corresponding to the ``Revision`` object.
+    :param session: Database session object.
+    :param include_history: Return all documents for revision history prior
+        and up to current revision, if ``True``.
     :raises: RevisionNotFound if the revision was not found.
     """
     session = session or get_session()
+    revision_documents = []
+
     try:
         revision = session.query(models.Revision)\
             .filter_by(id=revision_id)\
-            .one()\
-            .to_dict()
+            .one()
+        revision_documents = revision.to_dict()['documents']
+
+        if include_history:
+            older_revisions = session.query(models.Revision)\
+                .filter(models.Revision.created_at < revision.created_at)\
+                .order_by(models.Revision.created_at)\
+                .all()
+
+            map(lambda docs: revision_documents.extend(docs),
+                [r.to_dict()['documents'] for r in older_revisions])
     except sa_orm.exc.NoResultFound:
         raise errors.RevisionNotFound(revision=revision_id)
 
-    if 'deleted' not in filters:
-        filters.update({'deleted': False})
-
     filtered_documents = _filter_revision_documents(
-        revision['documents'], **filters)
+        revision_documents, **filters)
 
     return filtered_documents
 
@@ -316,6 +331,149 @@ def _filter_revision_documents(documents, **filters):
             filtered_documents.append(document)
 
     return filtered_documents
+
+
+def revision_diff_get(revision_id, comparison_revision_id):
+    """Generate the diff between two revisions.
+
+    Generate the diff between the two revisions `revision_id` and
+    `comparison_revision_id`. A basic comparison of the revisions in terms of
+    how the buckets involved have changed is generated. Only buckets with
+    existing documents in either of the two revisions in question will be
+    reported.
+
+    The ordering of the two revision ids is not important.
+
+    The differences include:
+
+        - "created": A bucket has been created between the revisions.
+        - "deleted": A bucket has been deleted between the revisions.
+        - "modified": A bucket has been modified between the revisions.
+        - "unmodified": A bucket remains unmodified between the revisions.
+
+    :param revision_id: ID of the first revision.
+    :param comparison_revision_id: ID of the second revision.
+    :returns: A dictionary, keyed with the bucket IDs, containing any of the
+        differences enumerated above.
+
+    Examples::
+
+        # GET /api/v1.0/revisions/6/diff/3
+        bucket_a: created
+        bucket_b: deleted
+        bucket_c: modified
+        bucket_d: unmodified
+
+        # GET /api/v1.0/revisions/0/diff/6
+        bucket_a: created
+        bucket_c: created
+        bucket_d: created
+
+        # GET /api/v1.0/revisions/6/diff/6
+        bucket_a: unmodified
+        bucket_c: unmodified
+        bucket_d: unmodified
+
+        # GET /api/v1.0/revisions/0/diff/0
+        {}
+    """
+    # Retrieve document history for each revision. Since `revision_id` of 0
+    # doesn't exist, treat it as a special case: empty list.
+    docs = (revision_get_documents(revision_id, include_history=True)
+            if revision_id != 0 else [])
+    comparison_docs = (revision_get_documents(comparison_revision_id,
+                                              include_history=True)
+                       if comparison_revision_id != 0 else [])
+
+    revision = revision_get(revision_id) if revision_id != 0 else None
+    comparison_revision = (revision_get(comparison_revision_id)
+                           if comparison_revision_id != 0 else None)
+
+    # Each dictionary below, keyed with the bucket's name, references the list
+    # of documents related to each bucket.
+    buckets = {}
+    comparison_buckets = {}
+    for doc in docs:
+        buckets.setdefault(doc['bucket_id'], [])
+        buckets[doc['bucket_id']].append(doc)
+    for doc in comparison_docs:
+        comparison_buckets.setdefault(doc['bucket_id'], [])
+        comparison_buckets[doc['bucket_id']].append(doc)
+
+    # Exclude buckets that have no shared documents in either `revision_id` or
+    # `comparison_revision_id`.
+    if not (revision_id == 0 or comparison_revision_id == 0):
+        endpoint_revision_docs = {(d['name'], d['schema']) for d in docs
+                                   if d['revision_id'] == revision_id}
+        endpoint_revision_docs.union(
+            {(d['name'], d['schema']) for d in comparison_docs
+              if d['revision_id'] == revision_id})
+
+        # Exclude buckets belonging to `revision_id`.
+        for bucket_name, bucket_docs in buckets.items():
+            found = False
+            for d in bucket_docs:
+                if (d['name'], d['schema']) in endpoint_revision_docs:
+                    found = True
+                    break
+            if not found:
+                buckets.pop(bucket_name, None)
+
+        # Exclude buckets belonging to `comparison_revision_id`.
+        for bucket_name, bucket_docs in comparison_buckets.items():
+            found = False
+            for d in bucket_docs:
+                if (d['name'], d['schema']) in endpoint_revision_docs:
+                    found = True
+                    break
+            if not found:
+                comparison_buckets.pop(bucket_name, None)
+
+    # `shared_buckets` references buckets shared by both `revision_id` and
+    # `comparison_revision_id` -- i.e. their intersection.
+    shared_buckets = set(buckets.keys()).intersection(
+        comparison_buckets.keys())
+    # `unshared_buckets` references buckets not shared by both `revision_id`
+    # and `comparison_revision_id` -- i.e. their non-intersection.
+    unshared_buckets = set(buckets.keys()).union(
+        comparison_buckets.keys()) - shared_buckets
+
+    result = {}
+
+    # If the list of documents for each bucket is indentical, then the result
+    # is "unmodified", else "modified".
+    for bucket_id in shared_buckets:
+        unmodified = (sorted(buckets[bucket_id]) ==
+                      sorted(comparison_buckets[bucket_id]))
+        result[bucket_id] = 'unmodified' if unmodified else 'modified'
+
+    for bucket_id in unshared_buckets:
+        # If neither revision has documents, then there's nothing to compare.
+        # This is always True for revision_id == comparison_revision_id == 0.
+        if not any([revision, comparison_revision]):
+            break
+        # Else if one revision == 0 and the other revision != 0, then the
+        # bucket has been created. Which is zero or non-zero doesn't matter.
+        elif not all([revision, comparison_revision]):
+            result[bucket_id] = 'created'
+        # Else if `revision` is newer than `comparison_revision`, then if the
+        # `bucket_id` isn't in the `revision` buckets, then it has been
+        # deleted. Otherwise it has been created.
+        elif revision['created_at'] > comparison_revision['created_at']:
+            if bucket_id not in buckets:
+                result[bucket_id] = 'deleted'
+            elif bucket_id not in comparison_buckets:
+                result[bucket_id] = 'created'
+        # Else if `comparison_revision` is newer than `revision`, then if the
+        # `bucket_id` isn't in the `revision` buckets, then it has been
+        # created. Otherwise it has been deleted.
+        else:
+            if bucket_id not in buckets:
+                result[bucket_id] = 'created'
+            elif bucket_id not in comparison_buckets:
+                result[bucket_id] = 'deleted'
+
+    return result
 
 
 ####################
