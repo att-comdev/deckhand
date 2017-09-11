d0c728cc8b667aefe1500738289b70f7b19765fc
Felipe Monteiro <felipe.monteiro@att.com>
2017-09-11 20:46:40 +0100

    [WIP] Bucket deletion implementation
    Change-Id: I6693bbb918cb672de315a66bb087de547df302d1

--- deckhand/tests/unit/db/test_revisions.py
+++ deckhand/tests/unit/db/test_revisions.py
@@ -37,13 +37,17 @@ class TestRevisions(base.TestDbBase):
                      for _ in range(4)]
         bucket_name = test_utils.rand_name('bucket')
         created_documents = self.create_documents(bucket_name, documents)
+        orig_revision_id = created_documents[0]['revision_id']
 
         # Update the last document.
         documents[-1]['data'] = {'foo': 'bar'}
         updated_documents = self.create_documents(
             bucket_name, documents, do_validation=False)
+        new_revision_id = updated_documents[0]['revision_id']
 
-        self.assertEqual(1, len(updated_documents))
+        # 4 documents should be returned: the updated doc along with the other
+        # 3 documents (unchanged) that accompanied the PUT request.
+        self.assertEqual(4, len(updated_documents))
         self.assertEqual(created_documents[-1]['bucket_id'],
                          updated_documents[0]['bucket_id'])
         self.assertNotEqual(created_documents[-1]['revision_id'],
@@ -52,8 +56,67 @@ class TestRevisions(base.TestDbBase):
         revision_documents = self.list_revision_documents(
             updated_documents[0]['revision_id'])
         self.assertEqual(4, len(revision_documents))
-        self.assertEqual(created_documents[:-1] + updated_documents,
-                         revision_documents)
+
+        self.assertEqual([orig_revision_id] * 3 + [new_revision_id],
+                         [d['revision_id'] for d in revision_documents])
+
+        self.assertEqual(
+            [(d['name'], d['schema'])
+             for d in (created_documents[:-1] + [updated_documents[-1]])],
+            [(d['name'], d['schema']) for d in revision_documents])
+
+    def test_recreate_with_no_changes(self):
+        documents = [base.DocumentFixture.get_minimal_fixture()
+                     for _ in range(1)]
+        bucket_name = test_utils.rand_name('bucket')
+        created_documents = self.create_documents(bucket_name, documents)
+        recreated_documents = self.create_documents(bucket_name, documents)
+
+        created_rev_id = created_documents[0].pop('revision_id')
+        recreated_rev_id = recreated_documents[0].pop('revision_id')
+        recreated_orig_rev_id = recreated_documents[0].pop('orig_revision_id')
+
+        for attr in ('data', 'metadata', 'name', 'schema'):
+            self.assertEqual(
+                created_documents[0][attr], recreated_documents[0][attr])
+        self.assertEqual(created_rev_id, recreated_orig_rev_id)
+        self.assertEqual(created_rev_id + 1, recreated_rev_id)
+
+        revision_documents = self.list_revision_documents(recreated_rev_id)
+        docs_rev_id = revision_documents[0].pop('revision_id')
+
+        for attr in ('data', 'metadata', 'name', 'schema'):
+            self.assertEqual(
+                revision_documents[0][attr], recreated_documents[0][attr])
+        self.assertEqual(recreated_orig_rev_id, docs_rev_id)
+        self.assertEqual(recreated_rev_id, docs_rev_id + 1)
+
+    def test_recreate_with_no_changes_different_buckets(self):
+        documents = [base.DocumentFixture.get_minimal_fixture()
+                     for _ in range(1)]
+        bucket_name = test_utils.rand_name('bucket')
+        alt_bucket_name = test_utils.rand_name('bucket')
+        created_documents = self.create_documents(bucket_name, documents)
+        recreated_documents = self.create_documents(alt_bucket_name, documents)
+
+        created_rev_id = created_documents[0].pop('revision_id')
+        recreated_rev_id = recreated_documents[0].pop('revision_id')
+        recreated_orig_rev_id = recreated_documents[0].pop('orig_revision_id')
+
+        for attr in ('data', 'metadata', 'name', 'schema'):
+            self.assertEqual(
+                created_documents[0][attr], recreated_documents[0][attr])
+        self.assertEqual(created_rev_id, recreated_orig_rev_id)
+        self.assertEqual(created_rev_id + 1, recreated_rev_id)
+
+        revision_documents = self.list_revision_documents(recreated_rev_id)
+        docs_rev_id = revision_documents[0].pop('revision_id')
+
+        for attr in ('data', 'metadata', 'name', 'schema'):
+            self.assertEqual(
+                revision_documents[0][attr], recreated_documents[0][attr])
+        self.assertEqual(recreated_orig_rev_id, docs_rev_id)
+        self.assertEqual(recreated_rev_id, docs_rev_id + 1)
 
     def test_list_with_validation_policies(self):
         documents = [base.DocumentFixture.get_minimal_fixture()
@@ -82,7 +145,7 @@ class TestRevisions(base.TestDbBase):
                                 for _ in range(3)]
             bucket_name = test_utils.rand_name('bucket')
             created_documents = self.create_documents(
-                bucket_name, document_payload)
+                bucket_name, document_payload, do_validation=False)
             all_created_documents.extend(created_documents)
             revision_id = created_documents[0]['revision_id']
             all_revision_ids.append(revision_id)
