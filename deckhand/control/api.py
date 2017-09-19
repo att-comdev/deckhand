f84e4c91eed96d19a07e1635685d8ada7eae6b0d
Felipe Monteiro <felipe.monteiro@att.com>
2017-09-19 16:30:11 +0100

    Revision diffing API
    This commit implements revision diffing API and the
    required back-end logic needed to realize it.
    
    Included in this commit:
    
      - implementation of revision diffing
      - unskip all revision diff functional tests
      - add additional functional tests for revision diffing
      - relevant unit tests
      - document comparison is performed using hashing as opposed
        to more inefficient, direct comparisons
    
    Change-Id: I0419ee9b8cf3fca6fe75818615d2338dc00b1003

--- deckhand/control/api.py
+++ deckhand/control/api.py
@@ -22,6 +22,7 @@ from deckhand.conf import config
 from deckhand.control import base
 from deckhand.control import buckets
 from deckhand.control import middleware
+from deckhand.control import revision_diffing
 from deckhand.control import revision_documents
 from deckhand.control import revision_tags
 from deckhand.control import revisions
@@ -64,6 +65,7 @@ def _get_routing_map():
         '/api/v1.0/bucket/[A-za-z0-9\-]+/documents': ['PUT'],
         '/api/v1.0/revisions': ['GET', 'DELETE'],
         '/api/v1.0/revisions/[A-za-z0-9\-]+': ['GET'],
+        '/api/v1.0/revisions/[A-za-z0-9\-]+/diff/[A-za-z0-9\-]+': ['GET'],
         '/api/v1.0/revisions/[A-za-z0-9\-]+/tags': ['GET', 'DELETE'],
         '/api/v1.0/revisions/[A-za-z0-9\-]+/tags/[A-za-z0-9\-]+': [
             'GET', 'POST', 'DELETE'],
@@ -73,7 +75,7 @@ def _get_routing_map():
     for route in ROUTING_MAP.keys():
         # Denote the start of the regex with "^".
         route_re = '^.*' + route
-        # Debite the end of the regex with "$". Allow for an optional "/" at
+        # Denote the end of the regex with "$". Allow for an optional "/" at
         # the end of each request uri.
         route_re = route_re + '[/]{0,1}$'
         ROUTING_MAP[route_re] = ROUTING_MAP.pop(route)
@@ -97,6 +99,8 @@ def start_api(state_manager=None):
         ('bucket/{bucket_name}/documents', buckets.BucketsResource()),
         ('revisions', revisions.RevisionsResource()),
         ('revisions/{revision_id}', revisions.RevisionsResource()),
+        ('revisions/{revision_id}/diff/{comparison_revision_id}',
+            revision_diffing.RevisionDiffingResource()),
         ('revisions/{revision_id}/documents',
             revision_documents.RevisionDocumentsResource()),
         ('revisions/{revision_id}/tags', revision_tags.RevisionTagsResource()),
