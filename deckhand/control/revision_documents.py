7e6ce26e540c4b1a6777033d263d9a58325e6b5e
Felipe Monteiro <felipe.monteiro@att.com>
2017-10-03 20:18:47 +0100

    DECKHAND-66: Document substitution implementation
    This PS implements documentation substitution and
    the rendered-documents endpoint. Each time the
    rendered-documents is queried, the documents for
    the reqeust revision_id dynamically undergo
    secret substitution.
    
    All functional tests related to secret substitution
    have been unskipped.
    
    Deckhand currently does not real testing for
    verifying that secret substitution works
    for encrypted documents. This will only happen
    when integration testing is added to Deckhand to
    test its interaction with Keystone and Barbican.
    
    Included in this PS:
      - basic implementation for secret substitution
      - introduction of jsonpath_ng for searching for and
        updating jsonpaths in documents
      - rendered-documents endpoint
      - unit tests
      - all relevant functional tests unskipped
      - additional bucket controller tests include RBAC
        tests and framework testing RBAC via unit tests
    
    Change-Id: I86f269a5b616b518e5f742a4005891412226fe2a

--- deckhand/control/revision_documents.py
+++ deckhand/control/revision_documents.py
@@ -19,6 +19,7 @@ from deckhand.control import base as api_base
 from deckhand.control import common
 from deckhand.control.views import document as document_view
 from deckhand.db.sqlalchemy import api as db_api
+from deckhand.engine import secrets_manager
 from deckhand import errors
 from deckhand import policy
 
@@ -26,7 +27,7 @@ LOG = logging.getLogger(__name__)
 
 
 class RevisionDocumentsResource(api_base.BaseResource):
-    """API resource for realizing CRUD endpoints for revision documents."""
+    """API resource for realizing revision documents endpoint."""
 
     view_builder = document_view.ViewBuilder()
 
@@ -66,3 +67,54 @@ class RevisionDocumentsResource(api_base.BaseResource):
         resp.status = falcon.HTTP_200
         resp.append_header('Content-Type', 'application/x-yaml')
         resp.body = self.to_yaml_body(self.view_builder.list(documents))
+
+
+class RenderedDocumentsResource(api_base.BaseResource):
+    """API resource for realizing rendered documents endpoint.
+
+    Rendered documents are also revision documents, but unlike revision
+    documents, they are finalized documents, having undergone secret
+    substitution and document layering.
+
+    Returns a multi-document YAML response containing all the documents
+    matching the filters specified via query string parameters. Returned
+    documents will have secrets substituted into them and be layered with
+    other documents in the revision, in accordance with the ``LayeringPolicy``
+    that currently exists in the system.
+    """
+
+    view_builder = document_view.ViewBuilder()
+
+    @common.sanitize_params([
+        'schema', 'metadata.name', 'metadata.label'])
+    def on_get(self, req, resp, sanitized_params, revision_id):
+        include_cleartext = policy.conditional_authorize(
+            'deckhand:list_cleartext_documents', req.context, do_raise=False)
+        include_encrypted = policy.conditional_authorize(
+            'deckhand:list_encrypted_documents', req.context, do_raise=False)
+
+        filters = sanitized_params.copy()
+        filters['metadata.storagePolicy'] = []
+        if include_cleartext:
+            filters['metadata.storagePolicy'].append('cleartext')
+        if include_encrypted:
+            filters['metadata.storagePolicy'].append('encrypted')
+
+        try:
+            documents = db_api.revision_get_documents(
+                revision_id, **filters)
+        except (errors.RevisionNotFound) as e:
+            raise falcon.HTTPNotFound(description=e.format_message())
+
+        # TODO(fmontei): Currently the only phase of rendering that is
+        # performed is secret substitution, which can be done in any randomized
+        # order. However, secret substitution logic will have to be moved into
+        # a separate module that handles layering alongside substitution once
+        # layering has been fully integrated into this endpoint.
+        secrets_substitution = secrets_manager.SecretsSubstitution(documents)
+        rendered_documents = secrets_substitution.substitute_all()
+
+        resp.status = falcon.HTTP_200
+        resp.append_header('Content-Type', 'application/x-yaml')
+        resp.body = self.to_yaml_body(
+            self.view_builder.list(rendered_documents))
