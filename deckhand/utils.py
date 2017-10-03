9348a5618ab87f2f90852f9613f1991baea173b1
Felipe Monteiro <felipe.monteiro@att.com>
2017-10-03 20:18:47 +0100

    DECKHAND-66: Document substitution implementation
    This PS implements documentation substitution and
    the rendered-documents endpoint. Each time the
    rendered-documents is queried, the documents for
    the reqeust revision_id dynamically undergo
    secret substitution.
    
    Change-Id: I86f269a5b616b518e5f742a4005891412226fe2a

--- deckhand/utils.py
+++ deckhand/utils.py
@@ -15,6 +15,10 @@
 import re
 import string
 
+import jsonpath_ng
+
+from deckhand import errors
+
 
 def to_camel_case(s):
     """Convert string to camel case."""
@@ -28,8 +32,8 @@ def to_snake_case(name):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 
-def multi_getattr(multi_key, dict_data):
-    """Iteratively check for nested attributes in the YAML data.
+def jsonpath_parse(document, jsonpath):
+    """Parse value given JSON path in the document.
 
     Check for nested attributes included in "dest" attributes in the data
     section of the YAML file. For example, a "dest" attribute of
@@ -42,21 +46,69 @@ def multi_getattr(multi_key, dict_data):
            bar:
                baz: <data_to_be_substituted_here>
 
-    :param multi_key: A multi-part key that references nested data in the
-        substitutable part of the YAML data, e.g. ".foo.bar.baz".
-    :param substitutable_data: The section of data in the YAML data that
+    :param document: The section of data in the YAML document that
         is intended to be substituted with secrets.
+    :param jsonpath: A multi-part key that references nested data in the
+        substitutable part of the YAML data, e.g. ".foo.bar.baz".
     :returns: nested entry in ``dict_data`` if present; else None.
     """
-    attrs = multi_key.split('.')
-    # Ignore the first attribute if it is "." as that is a self-reference.
-    if attrs[0] == '':
-        attrs = attrs[1:]
+    if jsonpath.startswith('.'):
+        jsonpath = '$' + jsonpath
 
-    data = dict_data
-    for attr in attrs:
-        if attr not in data:
-            return None
-        data = data.get(attr)
+    p = jsonpath_ng.parse(jsonpath)
+    matches = p.find(document)
+    if matches:
+        return matches[0].value
 
-    return data
+
+def jsonpath_replace(document, value, jsonpath, pattern=None):
+    """Update value in document at the path specified by ``jsonpath``.
+
+    If the nested path corresponding to ``jsonpath`` isn't found in the
+    document, the path is created as an empty ``{}`` for each iteration.
+
+    :param document: The section of data in the YAML document that
+        is intended to be substituted with secrets.
+    :param value: The new value for document[jsonpath].
+    :param jsonpath: A multi-part key that references nested data in the
+        substitutable part of the YAML data, e.g. ".foo.bar.baz".
+    :returns: nested entry in ``dict_data`` if present; else None.
+    """
+    document = document.copy()
+    if jsonpath.startswith('.'):
+        jsonpath = '$' + jsonpath
+
+    def _do_replace():
+        p = jsonpath_ng.parse(jsonpath)
+        p_to_change = p.find(document)
+
+        if p_to_change:
+            _value = value
+            if pattern:
+                to_replace = p_to_change[0].value
+                # value represents the value to inject into to_replace that
+                # matches the pattern.
+                _value = re.sub(pattern, value, to_replace)
+            return p.update(document, _value)
+
+    result = _do_replace()
+    if result:
+        return result
+
+    # A pattern requires us to look up the data located at document[jsonpath]
+    # and then figure out what re.match(document[jsonpath], pattern) is (in
+    # pseudocode). But raise an exception in case the path isn't present in the
+    # document and a pattern has been provided since it is impossible to do the
+    # look up.
+    if pattern:
+        raise errors.MissingDocumentPattern(data=document)
+
+    # However, Deckhand should be smart enough to create the nested keys in the
+    # document if they don't exist and a pattern isn't required.
+    doc = document
+    for path in jsonpath.split('.')[1:]:
+        if path not in doc:
+            doc.setdefault(path, {})
+        doc = doc.get(path)
+
+    return _do_replace()
