3f8ad70fca06a1bde6b9f538d09be35ce9d26916
Felipe Monteiro <felipe.monteiro@att.com>
2017-10-11 19:50:11 -0400

    Add auth middlware for keystone authentication
    This PS adds auth middleware middleware so Deckhand
    can check whether a keystone token is authenticated
    before proceeding with any requests. The pattern in
    deckhand.control.middleware is borrowed from
    Drydock and Shipyard.
    
    Change-Id: I6eeeb4a4d9ab1f1cc8fb338e5cc21136ab4d5684

---
+++ deckhand/control/middleware.py
@@ -0,0 +1,71 @@
+# Copyright 2017 AT&T Intellectual Property.  All other rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import falcon
+from oslo_config import cfg
+
+import deckhand.context
+
+CONF = cfg.CONF
+
+
+class AuthMiddleware(object):
+
+    def process_request(self, req, resp):
+        ctx = req.context
+
+        auth_status = req.get_header(
+            'X-SERVICE-IDENTITY-STATUS')  # Values include Confirmed or Invalid
+        service = True
+
+        if auth_status is None:
+            auth_status = req.get_header('X-IDENTITY-STATUS')
+            service = False
+
+        if auth_status == 'Confirmed':
+            # User Identity, unique within owning domain
+            ctx.user = req.get_header(
+                'X-SERVICE-USER-NAME') if service else req.get_header(
+                    'X-USER-NAME')
+            # Identity-service managed unique identifier
+            ctx.user_id = req.get_header(
+                'X-SERVICE-USER-ID') if service else req.get_header(
+                    'X-USER-ID')
+            # Identity service managed unique identifier of owning domain of
+            # user name
+            ctx.user_domain_id = req.get_header(
+                'X-SERVICE-USER-DOMAIN-ID') if service else req.get_header(
+                    'X-USER-DOMAIN-ID')
+            # Identity service managed unique identifier
+            ctx.project_id = req.get_header(
+                'X-SERVICE-PROJECT-ID') if service else req.get_header(
+                    'X-PROJECT-ID')
+            # Name of owning domain of project
+            ctx.project_domain_id = req.get_header(
+                'X-SERVICE-PROJECT-DOMAIN-ID') if service else req.get_header(
+                    'X-PROJECT-DOMAIN-NAME')
+            if service:
+                # Comma-delimited list of case-sensitive role names
+                ctx.add_roles(req.get_header('X-SERVICE-ROLES').split(','))
+            else:
+                ctx.add_roles(req.get_header('X-ROLES').split(','))
+
+            if req.get_header('X-IS-ADMIN-PROJECT') == 'True':
+                ctx.is_admin_project = True
+            else:
+                ctx.is_admin_project = False
+        elif CONF.allow_anonymous_access:
+            req.context = deckhand.context.get_context()
+        else:
+            raise falcon.HTTPUnauthorized()
