b2a6da3d65fa4e729850a93dd2c887ad31d53a62
Felipe Monteiro <felipe.monteiro@att.com>
2017-10-17 00:14:34 +0100

    [DNM] Validations API
    Change-Id: I53c79a6544f44ef8beab2600ddc8a3ea91ada903

--- deckhand/control/buckets.py
+++ deckhand/control/buckets.py
@@ -31,7 +31,13 @@ LOG = logging.getLogger(__name__)
 
 
 class BucketsResource(api_base.BaseResource):
-    """API resource for realizing CRUD operations for buckets."""
+    """API resource for realizing CRUD operations for buckets.
+
+    All concrete documents in the payload must successfully pass their
+    JSON schema validations. If an error occurs, first save the failed
+    validation policy along with the malformed documents for debugging
+    and then raise an exception.
+    """
 
     view_builder = document_view.ViewBuilder()
     secrets_mgr = secrets_manager.SecretsManager()
@@ -49,8 +55,13 @@ class BucketsResource(api_base.BaseResource):
 
         # NOTE: Must validate documents before doing policy enforcement,
         # because we expect certain formatting of the documents while doing
-        # policy enforcement.
-        validation_policies = self._create_validation_policies(documents)
+        # policy enforcement. If any documents fail basic schema validaiton
+        # raise an exception immediately.
+        doc_validator = document_validation.DocumentValidation(documents)
+        try:
+            validation_policy, with_errors = doc_validator.validate_all()
+        except deckhand_errors.InvalidDocumentFormat as e:
+            raise falcon.HTTPBadRequest(description=e.format_message())
 
         for document in documents:
             if document['metadata'].get('storagePolicy') == 'encrypted':
@@ -60,28 +71,17 @@ class BucketsResource(api_base.BaseResource):
 
         self._prepare_secret_documents(documents)
 
-        # Save all the documents, including validation policies.
-        documents_to_create = documents + validation_policies
+        if with_errors:
+            documents_to_create = [validation_policy]
+        else:
+            documents_to_create = documents + [validation_policy]
         created_documents = self._create_revision_documents(
-            bucket_name, list(documents_to_create))
+            bucket_name, documents_to_create)
 
         if created_documents:
             resp.body = self.view_builder.list(created_documents)
         resp.status = falcon.HTTP_200
         resp.append_header('Content-Type', 'application/x-yaml')
-
-    def _create_validation_policies(self, documents):
-        # All concrete documents in the payload must successfully pass their
-        # JSON schema validations. Otherwise raise an error.
-        try:
-            validation_policies = document_validation.DocumentValidation(
-                documents).validate_all()
-        except deckhand_errors.InvalidDocumentFormat as e:
-            # FIXME(fmontei): Save the malformed documents and the failed
-            # validation policy in the DB for future debugging, and only
-            # afterward raise an exception.
-            raise falcon.HTTPBadRequest(description=e.format_message())
-        return validation_policies
 
     def _prepare_secret_documents(self, secret_documents):
         # Encrypt data for secret documents, if any.
---
+++ deckhand/control/views/validation.py
@@ -0,0 +1,55 @@
+# Copyright 2017 AT&T Intellectual Property.  All other rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from deckhand.control import common
+from deckhand import types
+from deckhand import utils
+
+
+class ViewBuilder(common.ViewBuilder):
+    """Model validation API responses as a python dictionary."""
+
+    _collection_name = 'validations'
+
+    def list(self, validations):
+        return [self.show(v) for v in validations]
+
+    def list_entries(self, entries):
+        results = []
+        for idx, e in enumerate(entries):
+            results.append({'status': e['status'], 'id': idx})
+
+        return {
+            'count': len(entries),
+            'results': results
+        }
+ 
+    def show(self, validation):
+        default_validator = {
+            'name': types.DECKHAND_SCHEMA_VALIDATION,
+            'version': '1.0'
+        }
+        return {
+            'status': validation.get('status'),
+            'validator': validation.get('validator', default_validator)
+        }
+
+    def show_entry(self, entry):
+        return {
+            'name': entry.get('name'),
+            'status': entry.get('status'),
+            'createdAt': entry.get('createdAt'),
+            'expiresAfter': entry.get('expiresAfter'),
+            'errors': entry.get('errors')
+        }
