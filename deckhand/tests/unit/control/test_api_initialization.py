29e9f03ceec312f4b4f2f47bf2dc5b44857329f9
Felipe Monteiro <felipe.monteiro@att.com>
2017-10-17 00:14:34 +0100

    DECKHAND-80: Validations API Implementation
    The Validations API has been introduced to Deckhand, allowing users
    to register new validation results in Deckhand, as well as query
    the API for validation results for a revision. The validation results
    include a list of errors that occurred during document validation.
    
    All functional tests related to the API are now passing.
    
    The following endpoints have been implemented:
    
       * /api/v1.0/revisions/{revision_id}/validations
       * /api/v1.0/revisions/{revision_id}/validations/{validation_name}
       * /api/v1.0/revisions/{revision_id}/validations/{validation_name}/entries
       * /api/v1.0/revisions/{revision_id}/validations/{validation_name}/entries/{entry_id}
    
    Some back-end refactoring was needed to implement this API. In
    particular:
    
      - Added a new Validation sqlalchemy DB model
      - Introduced DataSchema handling to the engine.document_validation
        module so that registered schema validations can be used
      - Changed the way the result of the 'deckhand-schema-validation' internal
        validation is generated: it is now the amalgamation of all the
        internal and registered schema validations executed
      - Introduced rawquery generation so that raw SQL queries can be used to
        get results from DB
    
    Fixed following bug:
    
      - UniqueConstraint is now used to correctly generate unique constraints
        for sqlalchemy models that are supposed to be combinations of columns
    
    Change-Id: I53c79a6544f44ef8beab2600ddc8a3ea91ada903

--- deckhand/tests/unit/control/test_api_initialization.py
+++ deckhand/tests/unit/control/test_api_initialization.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 import inspect
+import os
+
 import mock
 
 from deckhand.control import api
@@ -22,6 +24,7 @@ from deckhand.control import revision_documents
 from deckhand.control import revision_tags
 from deckhand.control import revisions
 from deckhand.control import rollback
+from deckhand.control import validations
 from deckhand.control import versions
 from deckhand.tests.unit import base as test_base
 from deckhand import utils
@@ -31,13 +34,27 @@ class TestApi(test_base.DeckhandTestCase):
 
     def setUp(self):
         super(TestApi, self).setUp()
+        # Mock the API resources.
         for resource in (buckets, revision_diffing, revision_documents,
-                         revision_tags, revisions, rollback, versions):
+                         revision_tags, revisions, rollback, validations,
+                         versions):
             class_names = self._get_module_class_names(resource)
             for class_name in class_names:
                 resource_obj = self.patchobject(
                     resource, class_name, autospec=True)
                 setattr(self, utils.to_snake_case(class_name), resource_obj)
+
+        # Mock the location of the configuration files for API initialization.
+        curr_path = os.path.dirname(os.path.realpath(__file__))
+        repo_path = os.path.join(
+            curr_path, os.pardir, os.pardir, os.pardir, os.pardir)
+        temp_config_files = [
+            os.path.join(repo_path, 'etc', 'deckhand', 'deckhand.conf.sample'),
+            os.path.join(repo_path, 'etc', 'deckhand', 'deckhand-paste.ini')
+        ]
+        mock_get_config_files = self.patchobject(
+            api, '_get_config_files', autospec=True)
+        mock_get_config_files.return_value = temp_config_files
 
     def _get_module_class_names(self, module):
         class_names = [obj.__name__ for name, obj in inspect.getmembers(module)
@@ -74,8 +91,16 @@ class TestApi(test_base.DeckhandTestCase):
                       self.revision_tags_resource()),
             mock.call('/api/v1.0/rollback/{revision_id}',
                       self.rollback_resource()),
+            mock.call('/api/v1.0/revisions/{revision_id}/validations',
+                      self.validations_resource()),
+            mock.call('/api/v1.0/revisions/{revision_id}/validations/'
+                      '{validation_name}',
+                      self.validations_resource()),
+            mock.call('/api/v1.0/revisions/{revision_id}/validations/'
+                      '{validation_name}/{entry_id}',
+                      self.validations_resource()),
             mock.call('/versions', self.versions_resource())
-        ])
+        ], any_order=True)
 
         mock_db_api.drop_db.assert_called_once_with()
         mock_db_api.setup_db.assert_called_once_with()
