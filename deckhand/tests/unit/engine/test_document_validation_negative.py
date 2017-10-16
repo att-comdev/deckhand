f016f9732df7f083bb18ac029adad1a6e5446e42
Felipe Monteiro <felipe.monteiro@att.com>
2017-10-17 00:14:34 +0100

    DECKHAND-80: Validations API Implementation
    The Validations API has been introduced to Deckhand, allowing users
    to register new validation results in Deckhand, as well as query
    the API for validation results for a revision. The validation results
    include a list of errors that occurred during document validation.
    
    All functional tests related to the API are now passing.
    
    The following endpoints have been implemented:
    
       * /api/v1.0/revisions/{revision_id}/validations
       * /api/v1.0/revisions/{revision_id}/validations/{validation_name}
       * /api/v1.0/revisions/{revision_id}/validations/{validation_name}/entries
       * /api/v1.0/revisions/{revision_id}/validations/{validation_name}/entries/{entry_id}
    
    Some back-end refactoring was needed to implement this API. In
    particular:
    
      - Added a new Validation sqlalchemy DB model
      - Introduced DataSchema handling to the engine.document_validation
        module so that registered schema validations can be used
      - Changed the way the result of the 'deckhand-schema-validation' internal
        validation is generated: it is now the amalgamation of all the
        internal and registered schema validations executed
      - Introduced rawquery generation so that raw SQL queries can be used to
        get results from DB
    
    Fixed following bug:
    
      - UniqueConstraint is now used to correctly generate unique constraints
        for sqlalchemy models that are supposed to be combinations of columns
    
    Change-Id: I53c79a6544f44ef8beab2600ddc8a3ea91ada903

--- deckhand/tests/unit/engine/test_document_validation_negative.py
+++ deckhand/tests/unit/engine/test_document_validation_negative.py
@@ -15,6 +15,7 @@
 from deckhand.engine import document_validation
 from deckhand import errors
 from deckhand.tests.unit.engine import base as engine_test_base
+from deckhand import types
 
 
 class TestDocumentValidationNegative(
@@ -23,29 +24,42 @@ class TestDocumentValidationNegative(
 
     BASIC_ATTRS = (
         'schema', 'metadata', 'data', 'metadata.schema', 'metadata.name')
-    SCHEMA_ERR = ("The provided YAML failed schema validation. "
-                  "Details: '%s' is a required property.")
-    SCHEMA_ERR_ALT = ("The provided %s YAML failed schema validation. "
-                      "Details: '%s' is a required property.")
+    SCHEMA_ERR = "'%s' is a required property"
+
+    def setUp(self):
+        super(TestDocumentValidationNegative, self).setUp()
+        # Mock out DB module (i.e. retrieving DataSchema docs from DB).
+        self.patch('deckhand.db.sqlalchemy.api.document_get_all')
 
     def _test_missing_required_sections(self, properties_to_remove):
         for idx, property_to_remove in enumerate(properties_to_remove):
+            critical = property_to_remove in self.BASIC_ATTRS
+
             missing_prop = property_to_remove.split('.')[-1]
             invalid_data = self._corrupt_data(property_to_remove)
+            expected_err = self.SCHEMA_ERR % missing_prop
 
-            if property_to_remove in self.BASIC_ATTRS:
-                expected_err = self.SCHEMA_ERR % missing_prop
+            doc_validator = document_validation.DocumentValidation(
+                invalid_data)
+            if critical:
+                self.assertRaisesRegexp(
+                    errors.InvalidDocumentFormat, expected_err,
+                    doc_validator.validate_all)
             else:
-                expected_err = self.SCHEMA_ERR_ALT % (
-                    self.data['schema'], missing_prop)
-
-            # NOTE(fmontei): '$' must be escaped for regex to pass.
-            expected_err = expected_err.replace('$', '\$')
-
-            with self.assertRaisesRegex(errors.InvalidDocumentFormat,
-                                        expected_err):
-                document_validation.DocumentValidation(
-                    invalid_data).validate_all()
+                validations = doc_validator.validate_all()
+                self.assertEqual(1, len(validations))
+                self.assertEqual('failure', validations[0]['status'])
+                self.assertEqual({'version': '1.0', 'name': 'deckhand'},
+                                 validations[0]['validator'])
+                self.assertEqual(types.DECKHAND_SCHEMA_VALIDATION,
+                                 validations[0]['name'])
+                self.assertEqual(1, len(validations[0]['errors']))
+                self.assertEqual(self.data['metadata']['name'],
+                                 validations[0]['errors'][0]['name'])
+                self.assertEqual(self.data['schema'],
+                                 validations[0]['errors'][0]['schema'])
+                self.assertEqual(expected_err,
+                                 validations[0]['errors'][0]['message'])
 
     def test_certificate_key_missing_required_sections(self):
         self._read_data('sample_certificate_key')
