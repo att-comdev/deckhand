d1f36c938f5472c4fd683f0163463f7643fed562
Felipe Monteiro <felipe.monteiro@att.com>
2017-11-24 17:39:34 +0000

    Reset primary key back to 1 after deleting all revisions
    This PS resets the primary key for each table in the
    database back to 1 after deleting all revisions
    (i.e. DELETE /revisions) which effectively purges all
    the data in the database.
    
    Change-Id: Id64e01ea85d7098e261a56cf9eacdcce716b1f48

--- deckhand/db/sqlalchemy/api.py
+++ deckhand/db/sqlalchemy/api.py
@@ -608,14 +608,23 @@ def revision_get_all(session=None, **filters):
 
 
 def revision_delete_all(session=None):
-    """Delete all revisions.
+    """Delete all revisions and resets primary key index back to 1 for each
+    table in the database.
 
     :param session: Database session object.
     :returns: None
     """
-    session = session or get_session()
-    session.query(models.Revision)\
-        .delete(synchronize_session=False)
+    engine = get_engine()
+    if engine.name == 'postgresql':
+        # NOTE(fmontei): While cascade should delete all data from all tables,
+        # we also need to reset the index to 1 for each table.
+        for table in ['buckets', 'revisions', 'revision_tags', 'documents',
+                      'validations']:
+            engine.execute(
+                text("TRUNCATE TABLE revisions RESTART IDENTITY CASCADE;")
+                .execution_options(autocommit=True))
+    else:
+        raw_query("DELETE FROM revisions;")
 
 
 def _exclude_deleted_documents(documents):
