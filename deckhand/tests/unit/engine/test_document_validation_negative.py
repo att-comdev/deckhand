8c88fc95598dd20001671aab55c9fa74c5ce7f42
Felipe Monteiro <felipe.monteiro@att.com>
2017-12-15 02:32:58 +0000

    Clean up document validation module
    This PS cleans up the document validation module
    to make it more readable and also to fix the
    previous FIXME that was included inside the module:
    
        Remove this [*] once all Deckhand tests have been
        refactored to account for dynamic schema registeration via
        `DataSchema` documents. Otherwise most tests will fail.
    
    * Refers to the fact that a workaround was being used to
      get around the fact that unregistered document schemas
      could pass validation.
    
    This behavior has been changed so that, given a schema
    like foo/bar/v1, it must be registered via a DataSchema
    document with `metadata.name`=foo/bar/v1 for the
    schema to be detected by Deckhand and then validated
    against the appropriate documents.
    
    This PS reworks some tests to account for this new
    behavior.
    
    Change-Id: Ie75742b984b7ad392cb41decc203d42842050c80

--- deckhand/tests/unit/engine/test_document_validation_negative.py
+++ deckhand/tests/unit/engine/test_document_validation_negative.py
@@ -12,76 +12,93 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import mock
+
 from deckhand.engine import document_validation
 from deckhand import errors
-from deckhand.tests.unit.engine import base as engine_test_base
+from deckhand import factories
+from deckhand.tests.unit.engine import base as test_base
 from deckhand import types
 
 
-class TestDocumentValidationNegative(
-        engine_test_base.TestDocumentValidationBase):
+class TestDocumentValidationNegative(test_base.TestDocumentValidationBase):
     """Negative testing suite for document validation."""
 
     # The 'data' key is mandatory but not critical if excluded.
-    CRITICAL_ATTRS = (
-        'schema', 'metadata', 'metadata.schema', 'metadata.name')
-    SCHEMA_ERR = "'%s' is a required property"
+    exception_map = {
+        'metadata': errors.InvalidDocumentFormat,
+        'metadata.schema': errors.InvalidDocumentFormat,
+        'metadata.name': errors.InvalidDocumentFormat,
+        'schema': errors.InvalidDocumentFormat,
+    }
 
     def setUp(self):
         super(TestDocumentValidationNegative, self).setUp()
-        # Mock out DB module (i.e. retrieving DataSchema docs from DB).
-        self.patch('deckhand.db.sqlalchemy.api.document_get_all')
+        # Stub out the DB call for retrieving DataSchema documents.
+        self.patchobject(document_validation.db_api, 'revision_get_documents',
+                         lambda *a, **k: [])
 
-    def _test_missing_required_sections(self, properties_to_remove):
+    def _do_validations(self, document_validator, expected, expected_err):
+        validations = document_validator.validate_all()
+        self.assertEqual(2, len(validations))
+        # The DataSchema document itself should've validated
+        # successfully.
+        self.assertEqual('success', validations[0]['status'])
+        self.assertEqual('failure', validations[-1]['status'])
+        self.assertEqual({'version': '1.0', 'name': 'deckhand'},
+                         validations[-1]['validator'])
+        self.assertEqual(types.DECKHAND_SCHEMA_VALIDATION,
+                         validations[-1]['name'])
+        self.assertEqual(1, len(validations[-1]['errors']))
+        self.assertEqual(expected['metadata']['name'],
+                         validations[-1]['errors'][-1]['name'])
+        self.assertEqual(expected['schema'],
+                         validations[-1]['errors'][-1]['schema'])
+        self.assertEqual(expected_err,
+                         validations[-1]['errors'][-1]['message'])
+
+    def _test_missing_required_sections(self, document, properties_to_remove):
         for idx, property_to_remove in enumerate(properties_to_remove):
-            critical = property_to_remove in self.CRITICAL_ATTRS
-
             missing_prop = property_to_remove.split('.')[-1]
-            invalid_data = self._corrupt_data(property_to_remove)
-            expected_err = self.SCHEMA_ERR % missing_prop
+            invalid_data = self._corrupt_data(document, property_to_remove)
 
-            doc_validator = document_validation.DocumentValidation(
-                invalid_data)
-            if critical:
-                self.assertRaisesRegexp(
-                    errors.InvalidDocumentFormat, expected_err,
-                    doc_validator.validate_all)
+            exception_raised = self.exception_map.get(property_to_remove, None)
+            expected_err_msg = "'%s' is a required property" % missing_prop
+
+            dataschema_factory = factories.DataSchemaFactory()
+            dataschema = dataschema_factory.gen_test(
+                invalid_data.get('schema', ''), {})
+            payload = [dataschema, invalid_data]
+
+            doc_validator = document_validation.DocumentValidation(payload)
+            if exception_raised:
+                self.assertRaises(
+                    exception_raised, doc_validator.validate_all)
             else:
-                validations = doc_validator.validate_all()
-                self.assertEqual(1, len(validations))
-                self.assertEqual('failure', validations[0]['status'])
-                self.assertEqual({'version': '1.0', 'name': 'deckhand'},
-                                 validations[0]['validator'])
-                self.assertEqual(types.DECKHAND_SCHEMA_VALIDATION,
-                                 validations[0]['name'])
-                self.assertEqual(1, len(validations[0]['errors']))
-                self.assertEqual(self.data['metadata']['name'],
-                                 validations[0]['errors'][0]['name'])
-                self.assertEqual(self.data['schema'],
-                                 validations[0]['errors'][0]['schema'])
-                self.assertEqual(expected_err,
-                                 validations[0]['errors'][0]['message'])
+                self._do_validations(doc_validator, invalid_data,
+                                     expected_err_msg)
 
     def test_certificate_key_missing_required_sections(self):
-        self._read_data('sample_certificate_key')
-        properties_to_remove = self.CRITICAL_ATTRS + (
+        document = self._read_data('sample_certificate_key')
+        properties_to_remove = tuple(self.exception_map.keys()) + (
             'data', 'metadata.storagePolicy',)
-        self._test_missing_required_sections(properties_to_remove)
+        self._test_missing_required_sections(document, properties_to_remove)
 
     def test_certificate_missing_required_sections(self):
-        self._read_data('sample_certificate')
-        properties_to_remove = self.CRITICAL_ATTRS + (
+        document = self._read_data('sample_certificate')
+        properties_to_remove = tuple(self.exception_map.keys()) + (
             'data', 'metadata.storagePolicy',)
-        self._test_missing_required_sections(properties_to_remove)
+        self._test_missing_required_sections(document, properties_to_remove)
 
     def test_data_schema_missing_required_sections(self):
-        self._read_data('sample_data_schema')
-        properties_to_remove = self.CRITICAL_ATTRS + ('data', 'data.$schema',)
-        self._test_missing_required_sections(properties_to_remove)
+        document = self._read_data('sample_data_schema')
+        properties_to_remove = tuple(self.exception_map.keys()) + (
+            'data', 'data.$schema',)
+        self._test_missing_required_sections(document, properties_to_remove)
 
     def test_document_missing_required_sections(self):
-        self._read_data('sample_document')
-        properties_to_remove = self.CRITICAL_ATTRS + (
+        document = self._read_data('sample_document')
+        properties_to_remove = tuple(self.exception_map.keys()) + (
             'data',
             'metadata.layeringDefinition',
             'metadata.layeringDefinition.layer',
@@ -93,45 +110,59 @@ class TestDocumentValidationNegative(
             'metadata.substitutions.0.src.schema',
             'metadata.substitutions.0.src.name',
             'metadata.substitutions.0.src.path')
-        self._test_missing_required_sections(properties_to_remove)
+        self._test_missing_required_sections(document, properties_to_remove)
 
     def test_document_invalid_layering_definition_action(self):
-        self._read_data('sample_document')
-        corrupted_data = self._corrupt_data(
-            'metadata.layeringDefinition.actions.0.method', 'invalid',
-            op='replace')
+        document = self._read_data('sample_document')
+        missing_data = self._corrupt_data(
+            document, 'metadata.layeringDefinition.actions.0.method',
+            'invalid', op='replace')
         expected_err = "'invalid' is not one of ['replace', 'delete', 'merge']"
 
-        doc_validator = document_validation.DocumentValidation(corrupted_data)
-        validations = doc_validator.validate_all()
-        self.assertEqual(1, len(validations))
-        self.assertEqual('failure', validations[0]['status'])
-        self.assertEqual({'version': '1.0', 'name': 'deckhand'},
-                         validations[0]['validator'])
-        self.assertEqual(types.DECKHAND_SCHEMA_VALIDATION,
-                         validations[0]['name'])
-        self.assertEqual(1, len(validations[0]['errors']))
-        self.assertEqual(self.data['metadata']['name'],
-                         validations[0]['errors'][0]['name'])
-        self.assertEqual(self.data['schema'],
-                         validations[0]['errors'][0]['schema'])
-        self.assertEqual(expected_err,
-                         validations[0]['errors'][0]['message'])
+        # Ensure that a dataschema document exists for the random document
+        # schema via mocking.
+        dataschema_factory = factories.DataSchemaFactory()
+        dataschema = dataschema_factory.gen_test(document['schema'], {})
+        payload = [dataschema, missing_data]
+        doc_validator = document_validation.DocumentValidation(payload)
+        self._do_validations(doc_validator, document, expected_err)
 
     def test_layering_policy_missing_required_sections(self):
-        self._read_data('sample_layering_policy')
-        properties_to_remove = self.CRITICAL_ATTRS + (
+        document = self._read_data('sample_layering_policy')
+        properties_to_remove = tuple(self.exception_map.keys()) + (
             'data', 'data.layerOrder',)
-        self._test_missing_required_sections(properties_to_remove)
+        self._test_missing_required_sections(document, properties_to_remove)
 
     def test_passphrase_missing_required_sections(self):
-        self._read_data('sample_passphrase')
-        properties_to_remove = self.CRITICAL_ATTRS + (
+        document = self._read_data('sample_passphrase')
+        properties_to_remove = tuple(self.exception_map.keys()) + (
             'data', 'metadata.storagePolicy',)
-        self._test_missing_required_sections(properties_to_remove)
+        self._test_missing_required_sections(document, properties_to_remove)
 
     def test_validation_policy_missing_required_sections(self):
-        self._read_data('sample_validation_policy')
-        properties_to_remove = self.CRITICAL_ATTRS + (
+        document = self._read_data('sample_validation_policy')
+        properties_to_remove = tuple(self.exception_map.keys()) + (
             'data', 'data.validations', 'data.validations.0.name')
-        self._test_missing_required_sections(properties_to_remove)
+        self._test_missing_required_sections(document, properties_to_remove)
+
+    @mock.patch.object(document_validation, 'LOG', autospec=True)
+    def test_invalid_document_schema_generates_error(self, mock_log):
+        document = self._read_data('sample_document')
+        document['schema'] = 'foo/bar/v1'
+
+        doc_validator = document_validation.DocumentValidation(document)
+        doc_validator.validate_all()
+        self.assertRegex(
+            mock_log.error.mock_calls[0][1][0],
+            'The provided document schema %s is invalid.' % document['schema'])
+
+    @mock.patch.object(document_validation, 'LOG', autospec=True)
+    def test_invalid_document_schema_version_generates_error(self, mock_log):
+        document = self._read_data('sample_passphrase')
+        document['schema'] = 'deckhand/Passphrase/v5'
+
+        doc_validator = document_validation.DocumentValidation(document)
+        doc_validator.validate_all()
+        self.assertRegex(
+            mock_log.error.mock_calls[0][1][0],
+            'The provided document schema %s is invalid.' % document['schema'])
