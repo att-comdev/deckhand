b5c34173a374503139eaab5ce7418d5842078ae8
Felipe Monteiro <felipe.monteiro@att.com>
2017-12-20 18:05:05 +0000

    Sorting/filtering for rendered-documents
    This PS implements sorting and filtering for rendered-documents
    endpoint, adds additional validations for sorting, filtering
    and other layering scenarios, and updates rendered-documents
    and buckets documentation.
    
    Layering scenarios added:
      - Updating the LayeringPolicy with 2 layers in the layerOrder
        (down from 3) such that the site document should have its
        parent document recomputed as the global document.
      - A deletion action layering scenario (DH currently only has
        merge, replace scenarios in its funcitonal test suite.)
    
    Documentation updated:
      - clarify the access levels for buckets, which has been a
        source of confusion.
      - update api-ref documentation for rendered-documents
    
    Change-Id: Idb9b42351dfbdf75a19282c8478065e7564cfc26

--- deckhand/engine/document.py
+++ deckhand/engine/document.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import six
-
 
 class Document(object):
     """Object wrapper for documents.
@@ -40,10 +38,8 @@ class Document(object):
         concrete.
         """
         try:
-            abstract = self._inner['metadata']['layeringDefinition'][
-                'abstract']
-            return six.text_type(abstract) == 'True'
-        except KeyError:
+            return self._inner['metadata']['layeringDefinition']['abstract']
+        except Exception:
             return False
 
     def get_schema(self):
@@ -53,7 +49,10 @@ class Document(object):
         return self._inner['metadata']['name']
 
     def get_layer(self):
-        return self._inner['metadata']['layeringDefinition']['layer']
+        try:
+            return self._inner['metadata']['layeringDefinition']['layer']
+        except Exception:
+            return False
 
     def get_parent_selector(self):
         """Return the `parentSelector` for the document.
