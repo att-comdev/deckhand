37ea6a11b62080bcddfce6f487a644e5246ecffa
Felipe Monteiro <felipe.monteiro@att.com>
2017-12-30 22:24:16 +0000

    Simplify document wrapper class
    This PS simplifies the DocumentWrapper class by changing the way
    it is designed. The purpose of the class was to make it easier
    to retrieve nested dictionary attributes from a document. The class
    previously inherited from `object` meaning that the object could not
    directly be treated as a dictionary, complicating usage of the class.
    
    With this change, the class now inherits from a `dict` meaning that
    it can be manipulated the same way a dictionary can, while still
    able to return nested dictionary attributes without having to worry
    about exceptions getting thrown.
    
    Each property implemented by `DocumentWrapper` uses jsonpath_parse
    implements in `deckhand.utils` to retrieve nested attributes or
    else self.get() to retrieve first-level dictionary attributes.
    
    Change-Id: I1d73a79aa4c3117be31aab978c20258c1052ad6d

--- deckhand/engine/document.py
+++ deckhand/engine/document.py
@@ -12,115 +12,68 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from oslo_serialization import jsonutils as json
 
-class Document(object):
-    """Object wrapper for documents.
+from deckhand import utils
 
-    After "raw" documents undergo schema validation, they can be wrapped with
-    this class to allow nested dictionary entries to be quickly retrieved.
+
+class DocumentWrapper(dict):
+    """Wrapper for a document.
+
+    Useful for accessing nested dictionary keys without having to worry about
+    exceptions getting thrown.
     """
 
-    def __init__(self, data):
-        """Constructor for ``Document``.
-
-        :param data: Dictionary of all document data (includes metadata, data,
-            schema, etc.).
-        """
-        self._inner = data
-
-    def to_dict(self):
-        return self._inner
-
+    @property
     def is_abstract(self):
-        """Return whether the document is abstract.
+        return utils.jsonpath_parse(
+            self, 'metadata.layeringDefinition.abstract') is True
 
-        Not all documents contain this property; in that case they are
-        concrete.
+    @property
+    def schema(self):
+        return self.get('schema', None)
+
+    @property
+    def name(self):
+        return utils.jsonpath_parse(self, 'metadata.name')
+
+    @property
+    def layer(self):
+        return utils.jsonpath_parse(
+            self, 'metadata.layeringDefinition.layer')
+
+    @property
+    def parent_selector(self):
+        return utils.jsonpath_parse(
+            self, 'metadata.layeringDefinition.parentSelector') or {}
+
+    @property
+    def labels(self):
+        return utils.jsonpath_parse(self, 'metadata.labels') or {}
+
+    @property
+    def substitutions(self):
+        return utils.jsonpath_parse(self, 'metadata.substitutions') or []
+
+    @property
+    def actions(self):
+        return utils.jsonpath_parse(
+            self, 'metadata.layeringDefinition.actions') or []
+
+    @property
+    def children(self):
+        """Recursively retrieve all children.
+
+        Used in the layering module when calculating children for each
+        document.
+
+        :returns: List of nested children.
+        :rtype: Generator[:class:`DocumentWrapper`]
         """
-        try:
-            return self._inner['metadata']['layeringDefinition']['abstract']
-        except Exception:
-            return False
+        for child in self.get('children', []):
+            yield child
+            for granchild in child.get('children', []):
+                yield granchild
 
-    def get_schema(self):
-        return self._inner['schema']
-
-    def get_name(self):
-        return self._inner['metadata']['name']
-
-    def get_layer(self):
-        try:
-            return self._inner['metadata']['layeringDefinition']['layer']
-        except Exception:
-            return None
-
-    def get_parent_selector(self):
-        """Return the `parentSelector` for the document.
-
-        The topmost document defined by the `layerOrder` in the LayeringPolicy
-        does not have a `parentSelector` as it has no parent.
-
-        :returns: `parentSelcetor` for the document if present, else None.
-        """
-        try:
-            return self._inner['metadata']['layeringDefinition'][
-                'parentSelector']
-        except KeyError:
-            return None
-
-    def get_labels(self):
-        return self._inner['metadata']['labels']
-
-    def get_substitutions(self):
-        return self._inner['metadata'].get('substitutions', None)
-
-    def get_actions(self):
-        try:
-            return self._inner['metadata']['layeringDefinition']['actions']
-        except KeyError:
-            return []
-
-    def get_children(self, nested=False):
-        """Get document children, if any.
-
-        :param nested: Recursively retrieve all children for each child
-            document.
-        :type nested: boolean
-        :returns: List of children of type `Document`.
-        """
-        if not nested:
-            return self._inner.get('children', [])
-        else:
-            return self._get_nested_children(self, [])
-
-    def _get_nested_children(self, doc, nested_children):
-        for child in doc.get('children', []):
-            nested_children.append(child)
-            if 'children' in child._inner:
-                self._get_nested_children(child, nested_children)
-        return nested_children
-
-    def get(self, k, default=None):
-        return self.__getitem__(k, default=default)
-
-    def __getitem__(self, k, default=None):
-        return self._inner.get(k, default)
-
-    def __setitem__(self, k, val):
-        self._inner[k] = val
-
-    def __delitem__(self, k):
-        if self.__contains__(k):
-            del self._inner[k]
-
-    def __contains__(self, k):
-        return self.get(k, default=None) is not None
-
-    def __missing__(self, k):
-        return not self.__contains__(k)
-
-    def __repr__(self):
-        return '(%s, %s)' % (self.get_schema(), self.get_name())
-
-    def __str__(self):
-        return str(self._inner)
+    def __hash__(self):
+        return hash(json.dumps(self, sort_keys=True))
