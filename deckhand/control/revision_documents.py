99cf48dd3285c9b132cbe3efb86e8e3f4518f355
Felipe Monteiro <felipe.monteiro@att.com>
2018-02-15 22:14:51 +0000

    Fail fast on bad substitution input during layering
    This PS causes layering module to fail fast on malformed
    ``metadata.substitutions`` entry in a document by performing
    built-in schema validation when validate=True is passed to
    the DocumentLayering constructor. This kwarg is useful for
    when the layering module is called directly -- i.e. by
    Promenade or Pegleg. (The Deckhand server already performs
    document pre-validation during document ingestion so there
    is no need for documents stored inside layering to be
    re-validated again for rendered-documents endpoint.)
    
    Next, a new exception was added -- SubstitutionSourceNotFound
    -- which is raised when a substitution document is referenced
    by another document but isn't found.
    
    Finally, the previous exception raised by the
    secrets_manager module has been renamed to UnknownSubstitutionError
    which now raises a 500 instead of a 400 as this exception will
    most likely be due to an internal server error of some kind.
    
    Unit tests were added and documentation changes were made.
    
    Change-Id: Idfd91a52ef9ffd8f9b1c06c6b84c3405acab6f16

--- deckhand/control/revision_documents.py
+++ deckhand/control/revision_documents.py
@@ -115,12 +115,15 @@ class RenderedDocumentsResource(api_base.BaseResource):
         except (errors.InvalidDocumentLayer,
                 errors.InvalidDocumentParent,
                 errors.IndeterminateDocumentParent,
-                errors.UnsupportedActionMethod,
-                errors.MissingDocumentKey) as e:
+                errors.MissingDocumentKey,
+                errors.UnsupportedActionMethod) as e:
             raise falcon.HTTPBadRequest(description=e.format_message())
         except (errors.LayeringPolicyNotFound,
-                errors.SubstitutionFailure) as e:
+                errors.SubstitutionSourceNotFound) as e:
             raise falcon.HTTPConflict(description=e.format_message())
+        except errors.errors.UnknownSubstitutionError as e:
+            raise falcon.HTTPInternalServerError(
+                description=e.format_message())
 
         # Filters to be applied post-rendering, because many documents are
         # involved in rendering. User filters can only be applied once all
