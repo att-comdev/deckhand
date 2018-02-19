da36465629745e2e29482fb28b5c122ff2271390
Felipe Monteiro <felipe.monteiro@att.com>
2018-02-19 17:17:19 +0000

    Fix: Inject secret payload rather than reference into document
    This PS fixes Deckhand currently wrongly substituting the secret
    reference Barbican returns into documents, rather than the secret
    payload itself.
    
    Closes #19
    
    Change-Id: I1d4eed85ed336e83a777b4343f37b10c91038342

--- deckhand/control/buckets.py
+++ deckhand/control/buckets.py
@@ -67,7 +67,12 @@ class BucketsResource(api_base.BaseResource):
                     'deckhand:create_encrypted_documents', req.context)
                 break
 
-        self._prepare_secret_documents(documents)
+        try:
+            self._prepare_secret_documents(documents)
+        except deckhand_errors.BarbicanException as e:
+            LOG.error('An unknown exception occurred while trying to store '
+                      'a secret in Barbican.')
+            raise falcon.HTTPInternalServerError(description=six.text_type(e))
 
         created_documents = self._create_revision_documents(
             bucket_name, documents, validations)
