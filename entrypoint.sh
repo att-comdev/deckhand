8d01c9557723fc33125ce20cade26a392543361f
Bryan Strassner <bryan.strassner@gmail.com>
2018-03-27 08:54:05 -0500

    [WIP] [fix] Add entrypoint options
    Changes the entrypoint.sh options for uwsgi to include:
     -b 32768 : for larger header/url handling
     --die-on-term : for more 'normal'  handling of SIGTERM
     --lazy-apps : to delay init of python until after forking workers
     --master : to provide a master process for handling request dispatch
    
    The purpose of these changes is intended to avoid some crash behavior
    that is occuring when the process being forked has an open db connection.
    The --lazy-apps option should delay initialization. The other options are
    recommended by uwsgi documentation, specicially the --master option.
    The larger buffer size is not strictly recommended, but matters when large
    headers are included.
    The die-on-term option should provide better behavior in the container
    environment.
    
    Change-Id: I70510246576a8fb6aa216e7c9c7e97c1c9ab791c

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,15 +24,20 @@ DECKHAND_API_TIMEOUT=${DECKHAND_API_TIMEOUT:-"600"}
 DECKHAND_API_WORKERS=${DECKHAND_API_WORKERS:-"4"}
 # Threads per worker
 DECKHAND_API_THREADS=${DECKHAND_API_THREADS:-"1"}
+# The Deckhand configuration directory containing deckhand.conf
+DECKHAND_CONFIG_DIR=${DECKHAND_CONFIG_DIR:-"/etc/deckhand/deckhand.conf"}
 
 # Start deckhand application
 exec uwsgi \
-    --http :${PORT} \
-    -w deckhand.cmd \
+    -b 32768 \
     --callable deckhand_callable \
-    --http-timeout $DECKHAND_API_TIMEOUT \
+    --die-on-term \
     --enable-threads \
+    --http :${PORT} \
+    --http-timeout $DECKHAND_API_TIMEOUT \
     -L \
-    --pyargv "--config-file /etc/deckhand/deckhand.conf" \
+    --master \
+    --pyargv "--config-file ${DECKHAND_CONFIG_DIR}/deckhand.conf" \
     --threads $DECKHAND_API_THREADS \
-    --workers $DECKHAND_API_WORKERS
+    --workers $DECKHAND_API_WORKERS \
+    -w deckhand.cmd
