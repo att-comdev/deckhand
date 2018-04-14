888ceea6c79743932b926674e79ce0e62c18675d
Felipe Monteiro <felipe.monteiro@att.com>
2018-04-14 17:09:23 -0400

    [WIP] Add integration tests
    Change-Id: Ib5ae1b345b2a4bd579671ec4ae9a232c2e3887dc

---
+++ tools/common-tests.sh
@@ -0,0 +1,192 @@
+#!/usr/bin/env bash
+
+function log_section {
+    set +x
+    echo 1>&2
+    echo 1>&2
+    echo === $* === 1>&2
+    set -x
+}
+
+
+function deploy_postgre {
+    POSTGRES_ID=$(
+        sudo docker run \
+            --detach \
+            --publish :5432 \
+            -e POSTGRES_DB=deckhand \
+            -e POSTGRES_USER=deckhand \
+            -e POSTGRES_PASSWORD=password \
+                postgres:9.5
+    )
+
+    POSTGRES_IP=$(
+        sudo docker inspect \
+            --format='{{ .NetworkSettings.Networks.bridge.IPAddress }}' \
+                $POSTGRES_ID
+    )    
+}
+
+
+function cleanup_deckhand {
+    if [ -n "$POSTGRES_ID" ]; then
+        sudo docker stop $POSTGRES_ID
+    fi
+    if [ -n "$DECKHAND_ID" ]; then
+        sudo docker stop $DECKHAND_ID
+    fi
+    if [ -d "$CONF_DIR" ]; then
+        rm -rf $CONF_DIR
+    fi
+}
+
+
+trap cleanup_deckhand EXIT
+
+
+function gen_config {
+    CONF_DIR=$(mktemp -d -p $(pwd))
+    sudo chmod 777 -R $CONF_DIR
+
+    log_section Creating config file
+
+    export DECKHAND_TEST_URL=http://localhost:9000
+    export DATABASE_URL=postgresql+psycopg2://deckhand:password@$POSTGRES_IP:5432/deckhand
+    # Used by Deckhand's initialization script to search for config files.
+    export DECKHAND_CONFIG_DIR=$CONF_DIR
+
+    cp etc/deckhand/logging.conf.sample $CONF_DIR/logging.conf
+
+# Create a logging config file to dump everything to stdout/stderr.
+cat <<EOCONF > $CONF_DIR/logging.conf
+[loggers]
+keys = root, deckhand, error
+
+[handlers]
+keys = null, stderr, stdout
+
+[formatters]
+keys = simple, context
+
+[logger_deckhand]
+level = DEBUG
+handlers = stdout
+qualname = deckhand
+
+[logger_error]
+level = ERROR
+handlers = stderr
+
+[logger_root]
+level = WARNING
+handlers = null
+
+[handler_stderr]
+class = StreamHandler
+args = (sys.stderr,)
+formatter = context
+
+[handler_stdout]
+class = StreamHandler
+args = (sys.stdout,)
+formatter = context
+
+[handler_null]
+class = logging.NullHandler
+formatter = context
+args = ()
+
+[formatter_context]
+class = oslo_log.formatters.ContextFormatter
+
+[formatter_simple]
+format=%(asctime)s.%(msecs)03d %(process)d %(levelname)s: %(message)s
+EOCONF
+
+# Create a Deckhand config file with bare minimum options.
+cat <<EOCONF > $CONF_DIR/deckhand.conf
+[DEFAULT]
+debug = true
+publish_errors = true
+use_stderr = true
+# NOTE: allow_anonymous_access allows these functional tests to get around
+# Keystone authentication, but the context that is provided has zero privileges
+# so we must also override the policy file for authorization to pass.
+allow_anonymous_access = true
+
+[oslo_policy]
+policy_file = policy.yaml
+
+[barbican]
+
+[database]
+connection = $DATABASE_URL
+
+[keystone_authtoken]
+# NOTE(fmontei): Values taken from clouds.yaml. Values only used for
+# integration testing.
+#
+# clouds.yaml (snippet):
+#
+# username: 'admin'
+# password: 'password'
+# project_name: 'admin'
+# project_domain_name: 'default'
+# user_domain_name: 'default'
+# auth_url: 'http://keystone.openstack.svc.cluster.local/v3'
+
+username = admin
+password = password
+project_name = admin
+project_domain_name = Default
+user_domain_name = Default
+auth_url = http://keystone.openstack.svc.cluster.local/v3
+auth_type = password
+EOCONF
+
+# Only set up logging if running Deckhand via uwsgi. The container already has
+# values for logging.
+if [ -z "$DECKHAND_IMAGE" ]; then
+    sed '1 a log_config_append = '"$CONF_DIR"'/logging.conf' $CONF_DIR/deckhand.conf
+fi
+
+# Only set up logging if running Deckhand via uwsgi. The container already has
+# values for logging.
+if [ -z "$DECKHAND_IMAGE" ]; then
+    sed '1 a log_config_append = '"$CONF_DIR"'/logging.conf' $CONF_DIR/deckhand.conf
+fi
+
+    echo $CONF_DIR/deckhand.conf 1>&2
+    cat $CONF_DIR/deckhand.conf 1>&2
+
+    echo $CONF_DIR/logging.conf 1>&2
+    cat $CONF_DIR/logging.conf 1>&2
+
+    log_section Starting server
+}
+
+
+function gen_paste {
+    log_section Creating paste config without [filter:authtoken]
+    # NOTE(fmontei): Since this script does not currently support Keystone
+    # integration, we remove ``filter:authtoken`` from the ``deckhand_api``
+    # pipeline to avoid any kind of auth issues.
+    sed 's/authtoken api/api/' etc/deckhand/deckhand-paste.ini &> $CONF_DIR/deckhand-paste.ini
+}
+
+
+function gen_policy {
+    log_section Creating policy file with liberal permissions
+
+    policy_file='etc/deckhand/policy.yaml.sample'
+    policy_pattern="deckhand\:"
+
+    touch $CONF_DIR/policy.yaml
+
+    sed -n "/$policy_pattern/p" "$policy_file" \
+        | sed 's/^../\"/' \
+        | sed 's/rule\:[A-Za-z\_\-]*/@/' > $CONF_DIR/policy.yaml
+
+    echo $CONF_DIR/'policy.yaml' 1>&2
+    cat $CONF_DIR/'policy.yaml' 1>&2
+}
---
+++ tools/integration-tests.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+# Script intended for running Deckhand integration tests, where integration
+# is defined as the interaction between Deckhand and Keystone and Barbican.
+# Installation dependency is openstack-helm-infra.
+
+# TODO(fmontei): Use Ansible for all this.
+# NOTE(fmontei): May have to automate the following installation guide for CI:
+# https://docs.openstack.org/openstack-helm/latest/install/developer/requirements-and-host-config.html#host-configuration
+
+set -xe
+
+# Install required packages.
+sudo apt-get update
+sudo apt-get install --no-install-recommends -y \
+        ca-certificates \
+        git \
+        make \
+        jq \
+        nmap \
+        curl \
+        uuid-runtime \
+        ipcalc
+
+# Clone openstack-helm-infra and setup host and k8s.
+CURRENT_DIR="$(pwd)"
+: ${OSH_INFRA_PATH:="../openstack-helm-infra"}
+: ${OSH_PATH:="../openstack-helm"}
+
+if [ ! -d "$OSH_INFRA_PATH" ]; then
+    git clone https://git.openstack.org/openstack/openstack-helm-infra.git ../openstack-helm-infra
+fi
+
+if [ ! -d "$OSH_PATH" ]; then
+    git clone https://git.openstack.org/openstack/openstack-helm.git ../openstack-helm
+fi
+
+cd ${OSH_INFRA_PATH}
+make dev-deploy setup-host
+make dev-deploy k8s
+
+cd ${OSH_PATH}
+# Setup clients on the host and assemble the charts¶
+./tools/deployment/developer/common/020-setup-client.sh
+# Deploy the ingress controller
+./tools/deployment/developer/common/030-ingress.sh
+# Deploy NFS Provisioner
+./tools/deployment/developer/nfs/040-nfs-provisioner.sh
+# Deploy MariaDB
+./tools/deployment/developer/nfs/050-mariadb.sh
+# Deploy RabbitMQ
+./tools/deployment/developer/nfs/060-rabbitmq.sh
+# Deploy Memcached
+./tools/deployment/developer/nfs/070-memcached.sh
+# Deploy Keystone
+./tools/deployment/developer/nfs/080-keystone.sh
+
+# Deploy Barbican.
+# Pull images and lint chart
+make pull-images barbican
+
+# Deploy command
+: ${OSH_EXTRA_HELM_ARGS:=""}
+helm upgrade --install barbican ./barbican \
+    --namespace=openstack \
+    ${OSH_EXTRA_HELM_ARGS} \
+    ${OSH_EXTRA_HELM_ARGS_HORIZON}
+
+# Wait for deploy
+./tools/deployment/common/wait-for-pods.sh openstack
+
+# Validate deployment info
+helm status barbican
+
+cd ${CURRENT_DIR}
+source ${CURRENT_DIR}/tools/common-tests.sh
+# TODO(fmontei): Deploy PostgreSQL and Deckhand.
+# TODO(fmontei): Run tests.
+
+# TODO(fmontei): Use Keystone bootstrap override instead.
+interfaces=("admin" "public" "internal")
+
+if [ -z "$( openstack service list --format value | grep deckhand )" ]; then
+    openstack service create --enable --name deckhand deckhand
+fi
+
+for iface in ${interfaces[@]}; do
+    if [ -z "$( openstack endpoint list --format value | grep deckhand | grep $iface )" ]; then
+        openstack endpoint create --enable \
+            --region RegionOne \
+            deckhand $iface http://barbican.openstack.svc.cluster.local:9000/api/v1.0
+    fi
+done
+
+openstack service list | grep deckhand
+openstack endpoint list | grep deckhand
+
+# gen_config
+# gen_paste
+# gen_policy
+
+# log_section "Running Deckhand via Docker"
+# sudo docker run \
+#     --rm \
+#     --net=host \
+#     -v $CONF_DIR:/etc/deckhand \
+#     $DECKHAND_IMAGE alembic upgrade head &> $STDOUT
+# sudo docker run \
+#     --rm \
+#     --net=host \
+#     -p 9000:9000 \
+#     -v $CONF_DIR:/etc/deckhand \
+#     $DECKHAND_IMAGE &> $STDOUT &
+
+# # Give the server a chance to come up. Better to poll a health check.
+# sleep 5
+
+# DECKHAND_ID=$(sudo docker ps | grep deckhand | awk '{print $1}')
+# echo $DECKHAND_ID
