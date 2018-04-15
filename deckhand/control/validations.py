20cd612060f5d0f5556037f2d1819b3630d2e1fa
Felipe Monteiro <felipe.monteiro@att.com>
2018-04-15 13:36:19 -0400

    [feature] Endpoint for listing revision validations with details
    This patch set adds a new endpoint to the Validations API which allows
    for listing all validations for a given revision with details.
    The response body for GET /api/v1.0/{revision_id}/validations/detail
    looks like:
    
      ---
      count: 1
      next: null
      prev: null
      results:
        - name: promenade-site-validation
          url: https://deckhand/api/v1.0/revisions/4/validations/promenade-site-validation/entries/0
          status: failure
          createdAt: 2017-07-16T02:03Z
          expiresAfter: null
          expiresAt: null
          errors:
            - documents:
                - schema: promenade/Node/v1
                  name: node-document-name
                - schema: promenade/Masters/v1
                  name: kubernetes-masters
              message: Node has master role, but not included in cluster masters list.
    
    Note that the Validations API in general is currently missing fields
    like url (as well as next and prev references) which will be included
    in a follow up.
    
    This will enable Shipyard to avoid performing a quadratic number
    of API look ups when querying Deckhand's Validations API: [0].
    
    The policy enforced for this endpoint is deckhand:list_validations.
    
    APIImpact
    DocImpact
    
    [0] https://github.com/att-comdev/shipyard/blob/06b5e82ea881342572eb1c34a8d038b40fc20056/shipyard_airflow/control/configdocs/deckhand_client.py#L265
    
    Change-Id: I827e5f47bffb23fa16ee5c8a705058034633baed

--- deckhand/control/validations.py
+++ deckhand/control/validations.py
@@ -47,7 +47,6 @@ class ValidationsResource(api_base.BaseResource):
             raise falcon.HTTPNotFound(description=e.format_message())
 
         resp.status = falcon.HTTP_201
-        resp.append_header('Content-Type', 'application/x-yaml')
         resp.body = self.view_builder.show(resp_body)
 
     def on_get(self, req, resp, revision_id, validation_name=None,
@@ -62,7 +61,6 @@ class ValidationsResource(api_base.BaseResource):
             resp_body = self._list_all_validations(req, resp, revision_id)
 
         resp.status = falcon.HTTP_200
-        resp.append_header('Content-Type', 'application/x-yaml')
         resp.body = resp_body
 
     @policy.authorize('deckhand:show_validation')
@@ -104,3 +102,20 @@ class ValidationsResource(api_base.BaseResource):
 
         resp_body = self.view_builder.list(validations)
         return resp_body
+
+
+class ValidationsDetailsResource(api_base.BaseResource):
+    """API resource for listing revision validations with details."""
+
+    view_builder = validation_view.ViewBuilder()
+
+    @policy.authorize('deckhand:list_validations')
+    def on_get(self, req, resp, revision_id):
+        try:
+            entries = db_api.validation_get_all_entries(revision_id,
+                                                        val_name=None)
+        except errors.RevisionNotFound as e:
+            raise falcon.HTTPNotFound(description=e.format_message())
+
+        resp.status = falcon.HTTP_200
+        resp.body = self.view_builder.detail(entries)
